30b85b9f780f4370b5f163f71524e62929d767fd
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-20 11:39:22 +0000

    more relationships

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,3 +1,3 @@
 class Medication < ApplicationRecord
-  has_many :users
+  has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :bg_tests
-  has_many :medications
+  has_and_belongs_to_many :medications
   has_secure_password
   validates :email, presence: true, uniqueness: true
 end
