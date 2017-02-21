18eb4d8640cf24a59a3ddb434e31c9bef728298f
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-21 02:49:20 +0000

    added to user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = "Thank you for registering!"
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  # Using a private method to encapsulate the permissible parameters is
+  # just a good pattern since you'll be able to reuse the same permit
+  # list between create and update. Also, you can specialize this method
+  # with per-user checking of permissible attributes.
+    def user_params
+      params.require(:user).permit( :email, :password, :password_confirmation)
+    end
+end
+
 end
