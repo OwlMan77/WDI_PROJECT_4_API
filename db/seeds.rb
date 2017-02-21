dc7e2b78736b9916f5ec8ab1ceae2adc19581786
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-21 15:23:41 +0000

    added scss

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
 User.create([
-  {first_name:'Steve', last_name:'Redgrave', mail: 'Steveredgrave@famousdiabetics.com', password:'password', ideal_bg: 5.5 },
+  {first_name:'Steve', last_name:'Redgrave', email: 'Steveredgrave@famousdiabetics.com', password:'password', ideal_bg: 5.5 },
   {first_name:'Halle', last_name:'Berry', email: 'halleberry@famousdiabetics.com', password:'password', ideal_bg: 6.7 },
   {first_name:'Dorian', last_name:'Gregory', email: 'doriangregory@famousdiabetics.com', password:'password', ideal_bg: 7.2},
   {first_name: 'Bret', last_name: 'Michaels', email: 'bretmichaels@famousdiabetics.com', password:'password', ideal_bg: 8.2 }])
@@ -28,8 +28,3 @@ Medication.create([
     delivery_method: 'Pill'
   }
   ])
-
-BgTest.create({
-  bg_result: 3.2,
-  user_id: 1
-  })
