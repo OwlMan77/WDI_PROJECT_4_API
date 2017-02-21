6e9835660248b7479eb3956929ef54c9ed6474c3
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-21 19:50:59 +0000

    added User resource to User.rb to alllow for new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :bg_tests
   resources :medications
+  resources :user
   post "register", to: "authentications#register"
   post "login", to: "authentications#login"
   get "users/:id", to: 'users#show'
