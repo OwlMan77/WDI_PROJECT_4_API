d0282696572c8acb7e950825b135fe67fa896a7f
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-23 01:56:47 +0000

    added to bg_test_serializer to make time and date more presentable

--- app/serializers/bg_test_serializer.rb
+++ app/serializers/bg_test_serializer.rb
@@ -1,4 +1,10 @@
 class BgTestSerializer < ActiveModel::Serializer
   attributes :id, :bg_result, :date, :time
   has_one :user_id
+  def date
+    object.date.strftime("%m/%d/%Y")
+  end
+  def time
+    "#{object.time.hour}:#{object.time.min}"
+  end
 end
