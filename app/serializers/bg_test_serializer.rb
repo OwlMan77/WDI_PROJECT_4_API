2cb14261498fd41a642024698a4523ddd1d864ff
Owlman77 <jeanmarc.mcghee@gmail.com>
2017-02-23 17:26:33 +0000

    readble labels

--- app/serializers/bg_test_serializer.rb
+++ app/serializers/bg_test_serializer.rb
@@ -1,7 +1,13 @@
 class BgTestSerializer < ActiveModel::Serializer
-  attributes :bg_result, :date, :time
+  attributes :bg_result, :date, :time, :readTime, :readDate
   def date
     object.date
+  end
+  def readTime
+    object.time.strftime("%H:%M")
+  end
+  def readDate
+    object.date.strftime("%d/%m/%Y")
   end
   def time
     object.time
